3004ae86ff3f15dba1ca63ed1fb26a542812a09a
lizexiong <1043460476@qq.com>
2018-07-02 23:26:44 +0800

    代码优化market_list redis_change before

--- filereplace/FileReplace.py
+++ filereplace/FileReplace.py
@@ -16,24 +16,14 @@ class ConfigureInitial(object):
 		self.config_content_dict = ""
 
 
-		self._gw_rest = settings.gw_rest
-		self._post_mc = settings.post_mc
-		self._post_mc_trade = settings.post_mc_trade
-		self._deal = settings.deal
-		self._guard = settings.guard
-		self._quote = settings.quote
-		self._swirlyd = settings.swirlyd
-
-	# def open_file(self,configfile):
-	# 	return open(configfile)
-    #
-	# def close_file(self,configfile):
-	# 	self.open_file()
+	def open_file(self,configfile):
+		self.configcontent = open(configfile)
+		config_content_dict = json.loads(self.configcontent.read())
+		return config_content_dict
 
 	def gw_rest(self):
-		configcontent = open(self._gw_rest,'r+')
+		self.config_content_dict = self.open_file(settings.gw_rest)
 
-		self.config_content_dict = json.loads(configcontent.read(),)
 		self.config_content_dict['market_list'].extend(self.trading)
 
 		if self.quote_redis != None:
@@ -43,42 +33,80 @@ class ConfigureInitial(object):
 		if self.deal_redis != None:
 				self.config_content_dict['redis_server_quote_group'][single] = [[self.quote_redis,6379,""],[self.quote_redis,6379,""]]
 
-		self.close_new_file(self._gw_rest)
-		configcontent.close()
-
-
+		self.close_new_file(settings.gw_rest)
 
 	def post_mc(self):
-
-		configcontent = open(self._post_mc,'r+')
-
-		self.config_content_dict = json.loads(configcontent.read(),)
+		self.config_content_dict = self.open_file(settings.post_mc)
 
 		if self.quote_redis != None:
 			for single in self.trading:
 				self.config_content_dict['redis_server_quote_group'][single] = [self.quote_redis, 6379, ""]
 
-		self.close_new_file(self._post_mc)
-		configcontent.close()
+		self.close_new_file(settings.post_mc)
 
 	def deal(self):
-		configcontent = open(self._deal,'r+')
+		self.config_content_dict = self.open_file(settings.deal)
 
-		self.config_content_dict = json.loads(configcontent.read())
-
+		# for single in self.trading:
+		# 	if len(self.trading) < 1:
+		# 		self.config_content_dict['market_list'] = single
+		# 	else:
+		# 		self.config_content_dict['market_list'].append(single)
+		self.market_list(self.trading)
+		self.redis_change()
+		# if self.deal_redis != None:
+		# 	self.config_content_dict['redis_server_address_deal'] = self.deal_redis
 		
+		self.close_new_file(settings.deal)
+
+	def quote(self):
+		self.config_content_dict = self.open_file(settings.quote)
+
+		for single in self.trading:
+			if len(self.trading) < 1:
+				self.config_content_dict['market_list'] = single
+			else:
+				self.config_content_dict['market_list'].append(single)
+
+		if self.deal_redis != None:
+			self.config_content_dict['redis_server_address_deal'] = self.deal_redis
+		if self.quote_redis != None:
+			self.config_content_dict['redis_server_address_quote'] = self.quote_redis
+
+		self.close_new_file(settings.quote)
+
+	def guard(self):
+		self.config_content_dict = self.open_file(settings.guard)
+
+		for single in self.trading:
+			if len(self.trading) < 1:
+				self.config_content_dict['market_list'] = single
+			else:
+				self.config_content_dict['market_list'].append(single)
 
 
+	def market_list(self,trading):
+		for single in self.trading:
+			if len(self.trading) < 1:
+				self.config_content_dict['market_list'] = single
+			else:
+				self.config_content_dict['market_list'].append(single)
 
+	def redis_change(self,):
+		if self.deal_redis != None:
+			self.config_content_dict['redis_server_address_deal'] = self.deal_redis
+		if self.quote_redis != None:
+			self.config_content_dict['redis_server_address_quote'] = self.quote_redis
 
 
 	def close_new_file(self,configfile):
 		new_file = open(configfile + ".txt" ,'w+')
 		new_file.write(json.dumps(self.config_content_dict,sort_keys=True, indent=4,))
-
+		self.configcontent.close()
 			
 
-
-obj = ConfigureInitial(['BTCZX','ETHZX'],'10.10.01.1','123','192.168.10.1')
-obj.gw_rest()
-obj.post_mc()
+obj = ConfigureInitial(['BTCZX','ETHZX'],'10.10.01.1','123','192.168.10.1','8.8.8.8')
+# obj.gw_rest()
+# obj.post_mc()
+obj.deal()
+# obj.quote()
